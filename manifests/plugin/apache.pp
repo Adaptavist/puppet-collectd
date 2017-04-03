b0e81589ab8366cdc7d06ca2edc5d0a29673967c
Matt Hope <mhope@MattHope-MacBook-Pro.local>
2017-04-03 13:51:55 +0100

    lint fixes and made apache module optional

--- manifests/plugin/apache.pp
+++ manifests/plugin/apache.pp
@@ -1,12 +1,25 @@
 class collectd::plugin::apache(
-  $url = 'http://localhost/server-status',
+  $url        = 'http://localhost/server-status',
+  $package    = 'collectd-apache',
+  $use_plugin = true,
   ) {
+
+  # if this plugin is needed, use it
+  if (str2bool($use_plugin)) {
+    # if this is running on centos 7 install the additional package to provide apache support
+    if ($::osfamily == 'RedHat' and versioncmp($::operatingsystemrelease,'7') >= 0 and $::operatingsystem != 'Fedora') {
+      package { $package:
+        before => Service['collectd']
+      }
+    }
+  }
   collectd::plugin { 'apache':
-    type  => 'apache',
-    lines => [
+    type   => 'apache',
+    lines  => [
               '<Instance "apache">',
               "  URL \"${url}?auto\"",
               '</Instance>'
               ],
+    onlyif => $use_plugin,
   }
 }
